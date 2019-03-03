99112baac1efbc463dfca96f9199f3ac8e62c98c
c-p-dev <chris.patuasic@gmail.com>
2019-03-04 01:46:03 +0800

    background problems

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import bg from './assets/sample_background_texture.png';
 
 import RegisterPage from './pages/register/register';
 import LoginPage from './pages/login/login';
--- src/app-router.js
+++ src/app-router.js
@@ -5,23 +5,25 @@ import ErrorPage from './pages/error/error';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import './App.css';
+import './styles/custom.scss';
+import bg from './assets/sample_background_texture.png';
 
 class AppRouter extends Component {
   render() {
     return (
-        <BrowserRouter>
-            <div> 
-                <Switch>
-                    <Route path="/register" component={RegisterPage} exact/>
-                    <Route path="/login" component={LoginPage}  exact/>
-                    <Route path="/" component={LoginPage}  exact/>
-                    <Route component={ErrorPage} />
-                </Switch>
-            </div>
+        // <BrowserRouter>
+        //     <div> 
+        //         {/* <Switch> */}
+        //             <Route path="/register" component={RegisterPage} exact/>
+        //             <Route path="/login" component={LoginPage}  exact/>
+        //             {/* <Route path="/" component={LoginPage}  exact/> */}
+        //             {/* <Route component={ErrorPage} /> */}
+        //         {/* </Switch> */}
+        //     </div>
          
-           
-            
-        </BrowserRouter>        
+           <p>dd</p>
+        // <img src={bg} alt="Logo" />
+        // </BrowserRouter>        
     );
   }
 }
